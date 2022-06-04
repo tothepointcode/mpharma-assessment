151b6dcd518a46b3fbd41a050d79a9e57df60387
cobytab <terra.andoh@gmail.com>
2022-06-04 16:58:07 +0000

    Added functions to save and read from localstorage

--- components/shared.js
+++ components/shared.js
@@ -1,7 +1,17 @@
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
-export const ScreenWidth = Dimensions.get('screen').width;
-export const ScreenHeight = Dimensions.get('screen').height;
+export const ScreenWidth = Dimensions.get("screen").width;
+export const ScreenHeight = Dimensions.get("screen").height;
 
-export const dataAPIUrl = 'http://www.mocky.io/v2/5c3e15e63500006e003e9795';
+export const dataAPIUrl = "http://www.mocky.io/v2/5c3e15e63500006e003e9795";
 
+// local data storage
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const saveData = async (key, value) => {
+  await AsyncStorage.setItem(key, value);
+};
+
+export const fetchData = async (key) => {
+  return await AsyncStorage.getItem(key);
+};

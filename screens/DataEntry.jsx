75481cfad4628d08f7b7a0ba5f965ab3969eb742
cobytab <terra.andoh@gmail.com>
2022-06-04 20:16:45 +0000

    Addition to storage implemented

--- screens/DataEntry.jsx
+++ screens/DataEntry.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ActivityIndicator } from "react-native";
 
 // formik
@@ -8,15 +8,53 @@ import { Formik } from "formik";
 import { colors } from "./../components/colors";
 const { gray2 } = colors;
 
+// api route
+import { saveData } from "../components/shared";
+
+// product context
+import { ProductContext } from "./../components/Contexts/ProductContext";
+
+// uuid
+import { v4 as uuidv4 } from "uuid";
+
 // custom components
 import RegularText from "../components/Texts/RegularText";
 import MainContainer from "../components/Containers/MainContainer";
 import StyledTextInput from "../components/Inputs/StyledTextInput";
 import RegularButton from "../components/Buttons/RegularButton";
 
-const DataEntry = ({ route }) => {
-  const handleOnSubmit = async (values, setSubmitting) => {
-    // handle data addition or edit here
+const DataEntry = ({ route, navigation }) => {
+  // product context
+  const { products, setProducts } = useContext(ProductContext);
+
+  const handleOnSubmit = async ({ name, price }, setSubmitting) => {
+    try {
+      // handle data addition or edit here
+      const newData = {
+        id: uuidv4(),
+        name,
+        prices: [
+          {
+            id: uuidv4(),
+            price,
+            date: Date.now(),
+          },
+        ],
+      };
+
+      const newDataList = [...products, newData];
+
+      // update storages
+      await saveData("mPharmaProducts", JSON.stringify(newDataList));
+      await setProducts(newDataList);
+
+      // move to products page
+      navigation.navigate("Products");
+      setSubmitting(false);
+    } catch (error) {
+      alert("Adding failed: " + error);
+      setSubmitting(false);
+    }
   };
 
   return (

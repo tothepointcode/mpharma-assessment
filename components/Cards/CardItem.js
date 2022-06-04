b99c0afc8b6e3c5b681a957a7b851b1c353eb060
cobytab <terra.andoh@gmail.com>
2022-06-04 11:38:28 +0000

    feat: :construction: Created CardItem component
    To be rendered by the CardList component

---
+++ components/Cards/CardItem.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native";
+
+//colors
+import { colors } from "./../colors";
+const { gray1, accent } = colors;
+import RegularText from "../Texts/RegularText";
+import CardAvi from "./CardAvi";
+import { ScreenWidth } from "../shared";
+import CardDetail from "./CardDetail";
+
+const CardRow = styled.TouchableOpacity`
+  flex-direction: row;
+  width: 100%;
+  height: ${(ScreenWidth - 50) * 0.25}px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  background-color: ${gray1};
+  overflow: hidden;
+`;
+
+const RightView = styled.View`
+  height: 100%;
+  padding-vertical: 15px;
+  padding-horizontal: 15px;
+  width: ${(ScreenWidth - 50) * 0.65}px;
+`;
+
+const ProductTitle = styled(RegularText)`
+  color: ${accent};
+  font-weight: bold;
+  margin-bottom: 5px;
+  text-align: left;
+`;
+
+// placeholder image
+import placeholder from "./../../assets/imgs/drug_placeholder.jpg";
+
+const CardItem = (props) => {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate("Details", {
+      ...props,
+    });
+  };
+
+  return (
+    <CardRow onPress={handleOnPress}>
+      <CardAvi img={placeholder} />
+
+      <RightView>
+        <ProductTitle>{props.name}</ProductTitle>
+        <CardDetail
+          iconByIonicon={"cash"}
+          valuePreFix="GHS "
+          value={props?.prices[0].price}
+        />
+      </RightView>
+    </CardRow>
+  );
+};
+
+export default CardItem;

1e4577db455da859e508842a209246ca415d7974
cobytab <terra.andoh@gmail.com>
2022-06-04 20:15:17 +0000

    Assigned context to app through provider

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import 'react-native-get-random-values';
 
 // navigator
 import RootStack from "./navigators/RootStack";
 
+// product context
+import { ProductContext } from "./components/Contexts/ProductContext";
+
 export default function App() {
+  const [products, setProducts] = useState("");
+
   return (
-    <>
+    <ProductContext.Provider value={{ products, setProducts }}>
       <StatusBar style="light" />
       <RootStack />
-    </>
+    </ProductContext.Provider>
   );
 }

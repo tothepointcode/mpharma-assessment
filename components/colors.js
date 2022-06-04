58ac8f6483be48ddb589de9d4a09738fe8e86c95
cobytab <terra.andoh@gmail.com>
2022-06-04 10:00:17 +0000

    Declared the colors to be used in the app

---
+++ components/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  white: "#fff",
+  black: "#000",
+  gray1: "#334155",
+  gray2: "#0F172A",
+  gray3: "#CBD5E1",
+  neutral: "#F5F5F5",
+  accent: "#fbcd77",
+  success: "#22C55E",
+  fail: "#EF4444",
+};

7e13575ac6e9654aebebd4f0b2ac49feeb57871c
cobytab <terra.andoh@gmail.com>
2022-06-04 13:38:09 +0000

    feat: :construction: Modifier buttons added to details screen
    Edit and Delete buttons with placeholder onPress functions

--- screens/Details.jsx
+++ screens/Details.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components/native";
 
 // colors
 import { colors } from "./../components/colors";
-const { neutral, gray1, gray2, gray3, accent, success } = colors;
+const { neutral, gray1, gray2, gray3, accent, success, fail } = colors;
 
 // custom components
 import RegularText from "../components/Texts/RegularText";
@@ -11,6 +12,7 @@ import BigText from "../components/Texts/BigText";
 import MainContainer from "../components/Containers/MainContainer";
 import CardDetail from "../components/Cards/CardDetail";
 import PriceItem from "../components/Prices/PriceItem";
+import RegularButton from "../components/Buttons/RegularButton";
 import { ScreenWidth } from "../components/shared";
 
 import placeholder from "./../assets/imgs/drug_placeholder.jpg";
@@ -37,6 +39,19 @@ const SectionTitle = styled(BigText)`
   margin-bottom: 10px;
   text-align: left;
   font-size: 25px;
+`;
+
+const ModifiersView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+  border-radius: 20px;
+`;
+
+const ModifierButton = styled(RegularButton)`
+  background-color: ${gray1};
+  margin-top: 5px;
+  width: 45%;
 `;
 
 const Details = ({ navigation, route }) => {
@@ -69,6 +84,34 @@ const Details = ({ navigation, route }) => {
             prices.map((item, index) => {
               return <PriceItem key={index} {...item} />;
             })}
+
+          <SectionTitle
+            style={{ fontSize: 20, marginTop: 5 }}
+          >{`Modifiers`}</SectionTitle>
+
+          <ModifiersView>
+            <>
+              <ModifierButton
+                textStyle={{ color: accent, fontStyle: "italic" }}
+                disabled={true}
+                nPress={() => {
+                  // trigger edit
+                }}
+              >
+                Edit <Ionicons name="pencil" size={16} color={accent} />
+              </ModifierButton>
+
+              <ModifierButton
+                textStyle={{ color: accent, fontStyle: "italic" }}
+                disabled={true}
+                onPress={() => {
+                  // trigger delete
+                }}
+              >
+                Delete <Ionicons name="trash-bin" size={16} color={accent} />
+              </ModifierButton>
+            </>
+          </ModifiersView>
         </>
       </DetailsView>
     </MainContainer>

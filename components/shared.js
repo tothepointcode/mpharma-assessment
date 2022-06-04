f86ecdc5c9fe34d7d278d379e816bfb272633c6e
cobytab <terra.andoh@gmail.com>
2022-06-04 15:29:26 +0000

    Shared file created to store shared values

---
+++ components/shared.js
@@ -0,0 +1,7 @@
+import { Dimensions } from 'react-native';
+
+export const ScreenWidth = Dimensions.get('screen').width;
+export const ScreenHeight = Dimensions.get('screen').height;
+
+export const dataAPIUrl = 'http://www.mocky.io/v2/5c3e15e63500006e003e9795';
+
